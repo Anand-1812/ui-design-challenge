20167cfa29a4bbe6e95514578c81477e749107fb
Anand-1812 <anand.work2005@gmail.com>
2026-01-03 15:30:49 +0530

    grouping element

--- practice/components/Components.tsx
+++ practice/components/Components.tsx
@@ -1,29 +1,39 @@
-import React from 'react'
-import { cn } from '@/lib/utils'
+import React from "react"
+import { cn } from "@/lib/utils"
 
 const Components = () => {
   return (
-    <div className={
-      cn("w-full rounded-2xl min-h-100 bg-neutral-100",
-        "bg-[radial-gradient(var(--color-neutral-200)_1px,_transparent_1px)]",
-        "bg-size-[20px_20px]",
-        "p-8 flex items-center justify-center group"
-      )
-    }>
-      <div className={cn(
-        "size-40 rounded-2xl bg-neutral-100 border border-neutral-200",
-        "bg-[radial-gradient(var(--color-neutral-300)_1px,_transparent_1px)]",
-        "bg-size-[10px_10px]",
-        "shadown-2xl relative perspective-distant"
-      )}>
+    <div
+      className={cn(
+        "w-full min-h-[400px] rounded-2xl",
+        "bg-neutral-100",
+        "bg-[radial-gradient(var(--color-neutral-200)_1px,transparent_1px)]",
+        "bg-[length:20px_20px]",
+        "p-8 flex items-center justify-center",
+        "group"
+      )}
+    >
+      <div
+        className={cn(
+          "relative size-40 rounded-2xl",
+          "bg-neutral-100 border border-neutral-200",
+          "bg-[radial-gradient(var(--color-neutral-300)_1px,transparent_1px)]",
+          "bg-[length:10px_10px]",
+          "shadow-2xl",
+          "perspective-distance transition-colors duration-300",
+          "group-hover:bg-neutral-300 group-hover:border-neutral-300"
+        )}
+      >
         <img
-          src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?
-          w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGF2YXRhcnN8ZW58MHx8MHx8fDA%3D"
+          src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=500&auto=format&fit=crop&q=60"
+          alt="Avatar"
           className={cn(
-            "transition-transform duration-500 ease-in-out",
             "h-full w-full object-cover rounded-2xl",
-            "transform rotate-x-20 rotate-z-20 -rotate-y-20 translate-z-20",
-            "group:hover:rotate-x-0 group-hover:rotate-y-0 group-hover:rotate-z-0"
+            "transition-transform duration-300 [transition-timing-function:cubic-bezier(0.4,0,0.2,1)]",
+            "will-change-transform",
+            "[transform:rotateX(20deg)_rotateY(-20deg)_rotateZ(20deg)_translateZ(20px)]",
+            "group-hover:[transform:rotateX(0deg)_rotateY(0deg)_rotateZ(0deg)_translateZ(0px)]",
+            "group-hover:scale-85"
           )}
         />
       </div>
