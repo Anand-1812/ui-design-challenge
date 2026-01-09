4a4556cd8903815a758e707c9d64be74ad3eb8b5
Anand-1812 <anand.work2005@gmail.com>
2026-01-09 22:44:26 +0530

    some more desgins

---
+++ card/app/page.tsx
@@ -0,0 +1,11 @@
+import Card from "@/components/Card"
+
+const page = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-50">
+      <Card />
+    </div>
+  )
+}
+
+export default page
---
+++ card/components/Card.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils"
+
+const Card = () => {
+  return (
+    <div className={cn(
+      "w-[20rem] min-h-[26rem] h-[26rem] rounded-xl",
+      "shadow-[0_1px_1px_rgba(0,0,0,0.5),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)]",
+      "p-4 flex flex-col"
+    )}>
+      <h2 className="font-bold text-[20px]">Card Components</h2>
+      <p className="text-neutral-400 mt-2 text-[15px]">A collection of beautiful UI components, let's get on with it.</p>
+    </div>
+  )
+}
+
+export default Card
